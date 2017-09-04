f3da79b85406470146adea17f05ea513e26fb5be
PrateepN <31596845+PrateepN@users.noreply.github.com>
2017-09-04 15:20:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ restService.use(bodyParser.urlencoded({
 restService.use(bodyParser.json());
 
 restService.post('/echo', function(req, res) {
-    console.log(req.body.metadata.intentName);
+    console.log(req.body.result.parameters.PolicyNumber);
     if(req.body.metadata.intentName == "AddFolder")
     {
         var speech = "Inside";
