1586ce485be014e30eaa7e734e8aa1ce16a47c0a
PrateepN <31596845+PrateepN@users.noreply.github.com>
2017-09-04 15:14:49 +0530

    Add files via upload

---
+++ smarteco.py
@@ -0,0 +1,30 @@
+# movies.py
+
+from flask import Flask, render_template, requests
+import requests
+import json
+
+app = Flask(__name__)
+
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web', 
+        'done': False
+    }
+]
+
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
+
+if __name__ == '__main__':
+    app.run(debug=True)
