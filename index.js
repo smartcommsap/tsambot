b7cd080a97443d98c5fc5de25f17eb1f7db471fe
PrateepN <31596845+PrateepN@users.noreply.github.com>
2017-09-04 16:10:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,40 +23,9 @@ restService.post('/echo', function(req, res) {
 	if(req.body.result.parameters.NewFolderName)
 	{
 		
-	var oauth = new OAuth({
-    consumer: {
-      key: '6e83adcc-09b3-4514-bb4f-442cfa21c019!TradeDocsThunderhead@sapient.com.trial',
-      secret: 'ab97a83f-bc76-4784-a559-bac258fb7dde'
-    },signature_method: 'HMAC-SHA1',
-    hash_function: function(base_string, key) {
-      return crypto.createHmac('sha1', key).update(base_string).digest('base64');
-    }
-  });
-  
-  var request_data = {
-    url: 'https://na4.smartcommunications.cloud/one/oauth1/cms/v4/folders',
-	//url: 'https://na4.smartcommunications.cloud/one/',
-	//url: 'https://na4.smartcommunications.cloud/one/',
-    method: 'POST',
-    data: {
-      name: req.body.result.parameters.NewFolderName,
-	  parentId: req.body.result.parameters.ParentID
-    },
+	
+		console.log(req.body.result.parameters.ParentID);
 
-};
-console.log(req.body.result.parameters.ParentID);
-/**		
-request({
-    url: request_data.url,
-    method: request_data.method,
-    form: request_data.data,
-    headers: oauth.toHeader(oauth.authorize(request_data))
-}, function(error, response, body) {
-    if (error) console.error(error);
-    //data1 = JSON.parse(response);
-    console.log(body);
-});
-**/
 	}
 	else{
 	
