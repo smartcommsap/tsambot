7bc88cf72337dda6996bfe05d43075a28a1920d5
PrateepN <31596845+PrateepN@users.noreply.github.com>
2017-09-05 10:17:44 +0530

    Add files via upload

--- smarteco.py
+++ smarteco.py
@@ -9,22 +9,17 @@ app.secret_key = '1006d5d50a924cbc89ecb5fc9e987652'
 
 
 
-@app.route('/getkeywords', methods = ['GET','POST'])
+@app.route('/echo', methods = ['GET','POST'])
 def getkeywords():
     
     import simplejson as j
     if request.method=="POST":
         path=request.form['text']
-        #print path 
-        jso = getKeywordsFromText(path)
-           
+        print path 
+        # 	jso = getKeywordsFromText(path)
         return jsonify(j.loads(jso))
-        
-        
-    else:
-        return redirect(url_for('home'))
     
 
 if __name__ == '__main__':
-    #app.run(host= '10.207.60.126',port=5000)
-    app.run(debug=True)
+    app.run(host= 'https://api.api.ai/v1/query?v=20150910',port=8000)
+    #app.run(debug=True)
