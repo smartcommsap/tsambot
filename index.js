3881309df57a7096f234fe28495d6d59836317a6
PrateepN <31596845+PrateepN@users.noreply.github.com>
2017-09-20 22:56:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -110,6 +110,15 @@ restService.post('/echo', function(req, res) {
 		}
 		
 	}
+	else
+	{
+		data= {
+      		"resId": "158100159",
+	  "rootElementName": "SCBOTRequest",
+	  "sampleData": "PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz48U0NCT1RSZXF1ZXN0PjxQb2xpY3lOdW1iZXI+cG9sMTIzPC9Qb2xpY3lOdW1iZXI+PFBvbGljeVByZW1pdW0+MTIzLjQ1PC9Qb2xpY3lQcmVtaXVtPjxRdW90ZU51bWJlcj5xdW90ZTEyMzwvUXVvdGVOdW1iZXI+PFF1b3RlUHJlbWl1bT4zNDUuNDU8L1F1b3RlUHJlbWl1bT48Q3VzdG9tZXJOYW1lPkpvaG4gRG9lPC9DdXN0b21lck5hbWU+PEFnZW5jeU5hbWU+UlRHIENvbnN1bHRhbnRzPC9BZ2VuY3lOYW1lPjxBZ2VuY3lQaG9uZU5vPjEyMy01NjQtMjMyPC9BZ2VuY3lQaG9uZU5vPjwvU0NCT1RSZXF1ZXN0Pg=="
+	};
+	req_url="https://192.168.0.102/smartcomms/oauth1/api/v4/listMissingFields";
+	}
 console.log("Outside Generate Document POL URL"+req_url);
 var request_vars =  {	
 	url: req_url,
