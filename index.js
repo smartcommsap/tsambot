1eedcc99131ef3f0bf7e86a208146e2940cb424c
PrateepN <31596845+PrateepN@users.noreply.github.com>
2017-09-27 14:24:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,14 +5,12 @@ restService.set('port', (process.env.PORT || 8000));
 var request = require('request');
 var xmldom = require('xmldom').DOMParser;
 var S = require('string');
-
 var encodeUrl = require('encodeurl');
 const jsxml = require("node-jsxml");
 
 //Added for Oauth1 authorization
 var OAuth = require('oauth-1.0a');
 var crypto = require('crypto');
-
 
 //For base64 utf-8 Encoding/Decoding
 var base64 = require('base-64');
@@ -149,49 +147,59 @@ request({
 
 
 }
-else if(req.body.result.parameters.DocNumber && req.body.result.parameters.DocNumber!="")
+else if(req.body.result.parameters.RequestType && req.body.result.parameters.RequestType!="")
 {
-
-
 //Sample Data value
-var premium="1234.56";
+var amount1="20.56";
 var agencyName="ABC Consultants";
 var agencyPhoneNo="123-564-232";
+var amount2="50.56";
 
 //reset for every new echo
+var requestType=req.body.result.parameters.RequestType;
+var resourceId="";
 var resourceVersionId="";
-var transactionType="";
+var transactionType="Policy";
 var docNumber="";
 var finalText="";
-
-var docNumberTest=req.body.result.parameters.DocNumber;
-
-var docType=docNumberTest.toString().substr(0,3).toUpperCase();
-console.log("docType: "+docType);
-
-if(docType=="POL" || docType=="QUO")
-{
-	var resourceId="157761703";
-	docNumber=docNumberTest; //Replace here
-	if(docType=="POL")
+var vehicle1="";
+var vehicle2="";
+var age="";
+	if(requestType.toString()=="showmonthlypremium")
 	{
-		transactionType="policy";
+		resourceId="157761703";
+		docNumber=req.body.result.parameters.DocNumber;
+	}	
+	else if(requestType.toString()=="updatebeneficiary")
+	{
+		resourceId="157762924";
+	}
+	else if(requestType.toString()=="petinsurance")
+	{
+		resourceId="157762933";
+	}
+	else if(requestType.toString()=="premiumfornewdriver" && req.body.result.parameters.Age.amount && req.body.result.parameters.Age.amount!="")
+	{
+		if(req.body.result.parameters.Age.amount>21)
+		{
+			resourceId="157762939";
+		}
+		else
+		{
+			resourceId="157762977";
+		}
+		age=req.body.result.parameters.Age.amount.toString()+" year old";
+	}
+	else if(requestType.toString()=="newpremiumforvehicle" && req.body.result.parameters.ConfirmDetails.toString().toUpperCase=="Y")
+	{
+		resourceId="157762557";
+		vehicle1=req.body.result.parameters.Vehicle1;
+		vehicle2=req.body.result.parameters.Vehicle2;
 	}
 	else
 	{
-		transactionType="quote";
+		speechText="Either you did not wish to continue further or we could not find the response for your request. Please try again or contact the support team."
 	}
-	
-
-var oauth = new OAuth({
-    consumer: {
-      key: '6e83adcc-09b3-4514-bb4f-442cfa21c019!pnath2@sapient.com.trial',
-      secret: 'ab97a83f-bc76-4784-a559-bac258fb7dde'
-    },signature_method: 'HMAC-SHA1',
-    hash_function: function(base_string, key) {
-      return crypto.createHmac('sha1', key).update(base_string).digest('base64');
-    }
-  });
 
 //Code to retrieve latest resource version id -- starts
 console.log("retrieve latest resource version id");
@@ -262,10 +270,14 @@ request({
 	
 	speechText=finalText;
 	speechText=S(speechText).replaceAll("<TransactionType>", transactionType).toString();
-	speechText=S(speechText).replaceAll("<DocNumber>", docNumber).toString();
-	speechText=S(speechText).replaceAll("<Premium>", premium).toString();
+	speechText=S(speechText).replaceAll("<PolicyNumber>", docNumber).toString();
+	speechText=S(speechText).replaceAll("<Amount1>", amount1).toString();
+	speechText=S(speechText).replaceAll("<Amount2>", amount2).toString();
 	speechText=S(speechText).replaceAll("<AgencyName>", agencyName).toString();
 	speechText=S(speechText).replaceAll("<AgencyPhoneNo>", agencyPhoneNo).toString();
+	speechText=S(speechText).replaceAll("<Vehicle1>", vehicle1).toString();
+	speechText=S(speechText).replaceAll("<Vehicle2>", vehicle2).toString();
+	speechText=S(speechText).replaceAll("<Age>", age).toString();
 	console.log(speechText);
 	
 		return res.json({
@@ -286,6 +298,15 @@ request({
 		});
 	}
 	}
+	else
+	{
+			speechText="Faced some internal issue. Please check with your admin.";
+			return res.json({
+			speech: speechText,
+			displayText: speechText,
+			source: 'webhook-echo-sample'
+		});
+	}
 });
 
 //Code to getContent for resourceVersionId -- ends
@@ -294,17 +315,7 @@ request({
 
 
 
-}
-else
-{			speechText="The details provided are not valid for any existing policy/quote.";
-			return res.json({
-			speech: speechText,
-			displayText: speechText,
-			source: 'webhook-echo-sample'
-			//Set Speech Text here for non POL/QUO
-});
 
-}
 }
 else if(req.body.result.parameters.GroupName && req.body.result.parameters.GroupName!="")
 {
